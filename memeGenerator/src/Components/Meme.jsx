a40cff5e191d478fc04f07c3f894f07d71208f11
Manuel Martins <37194324+manel27@users.noreply.github.com>
2023-02-01 17:44:45 +0000

    accesses random urls from memesData

--- memeGenerator/src/Components/Meme.jsx
+++ memeGenerator/src/Components/Meme.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import memesData from "../assets/memesData"
+
+function randomImg(){
+    const randIndex = Math.floor(Math.random()*memesData.data.memes.length)
+    console.log(memesData.data.memes[randIndex].url)
+}
 
 export default function Meme(){
     return(
         <main>
-            <form>
+            <div className='form'>
                 <input type = "text" placeholder = "Shut up"/>
                 <input type = "text" placeholder = "and take my money"/>
-                <button className = "submitButton">Get a new meme image</button>
-            </form>
+                <button className = "submitButton" 
+                    onClick = {randomImg}
+                >
+                    Get a new meme image
+                </button>
+            </div>
         </main>
     )
 }
