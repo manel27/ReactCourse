9dbc02c7bbf20468df596fde133736f02a78b153
Manuel Martins <37194324+manel27@users.noreply.github.com>
2023-02-08 17:42:50 +0000

    generator displays different memes using state

--- memeGenerator/src/Components/Meme.jsx
+++ memeGenerator/src/Components/Meme.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import memesData from "../assets/memesData"
 
-function randomImg(){
-    const randIndex = Math.floor(Math.random()*memesData.data.memes.length)
-    console.log(memesData.data.memes[randIndex].url)
-}
+    /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+
+
 
 export default function Meme(){
+
+    let [memeImage,randomImg] = React.useState("")
+
+    function changeMeme(){
+        const randIndex = Math.floor(Math.random()*memesData.data.memes.length)
+        //console.log(memesData.data.memes[randIndex].url)
+        randomImg(prevMeme => memesData.data.memes[randIndex].url)
+    }
+
     return(
         <main>
             <div className='form'>
                 <input type = "text" placeholder = "Shut up"/>
                 <input type = "text" placeholder = "and take my money"/>
                 <button className = "submitButton" 
-                    onClick = {randomImg}
+                    onClick = {changeMeme}
                 >
                     Get a new meme image
                 </button>
+            </div>
+            <div className='imgContainer'>
+                <img src = {memeImage} className='memeImg'/>
             </div>
         </main>
     )
